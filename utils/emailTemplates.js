542fad156742ae283858719c096d04ccaa5a9dc2
Aditi Rathi <146621417+aditihrathi@users.noreply.github.com>
2024-11-13 15:47:26 -0500

    Update emailTemplates.js

--- utils/emailTemplates.js
+++ utils/emailTemplates.js
@@ -1,4 +1,4 @@
-const imageUrl = 'https://i.imgur.com/6LYRbGN.png';
+const imageUrl = 'https://i.imgur.com/UNDtO5x.png';
 
 function generateEmailTemplate(rsvpData) {
   if (rsvpData.attendance === 'yes') {
